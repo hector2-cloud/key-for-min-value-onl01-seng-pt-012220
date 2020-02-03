2ac417fed3ec19a97c8d0a3d15df3eb06d0e03aa
awesome-writer-4373 <hectoraperez1999@gmail.com>
2020-02-03 17:53:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(hash)
   current_minimum= nil 
   current_key_assoc_w_min_val=nil
-  hash.each do |name,number
+  hash.each do |name,number|
   if current_minimum==nil
     current_minimum=1
     current_key_assoc_w_min_val=adam
