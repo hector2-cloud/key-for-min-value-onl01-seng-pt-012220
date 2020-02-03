286ae8ee909e4db8945e72d31af7a52853df50d5
awesome-writer-4373 <hectoraperez1999@gmail.com>
2020-02-03 18:05:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,17 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-  current_minimum= nil 
-  current_key_assoc_w_min_val=nil
+  
   hash.each do |name,number|
-  if current_minimum==nil
-    current_minimum=[1]
-    current_key_assoc_w_min_val=[:adam]
-  else
-    if current_minimum<0
-      current_minimum=[1] 
-      current_key_assoc_w_min_val=[:adam]
-end
-end
-end
-end
+  if number<2 
+    puts 
